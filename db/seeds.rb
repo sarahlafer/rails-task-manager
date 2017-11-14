4bddf44a7a94314c3ae6d3a8f9d9d634287d33d2
Sarah Lafer <sarah_lafer@yahoo.com>
2017-11-14 14:38:40 +0100

    My Task Manager

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Task.create!(
+  content: "Laundry",
+  date_of_creation: Date.today
+  )
+
+Task.create!(
+  content: "Hoover",
+  date_of_creation: Date.yesterday
+  )
